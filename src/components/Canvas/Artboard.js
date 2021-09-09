07f8ec76b9ff3a64f21ef6ecc5a1ac13d0b9b227
JanEuster <jan.eusterschulte@rhgy.de>
2021-09-09 23:06:05 +0200

    add basic ui of toolbox

--- src/components/Canvas/Artboard.js
+++ src/components/Canvas/Artboard.js
@@ -1,88 +1,95 @@
 import BaseObject from "./Objects/BaseObject.js";
 
 class Artboard {
-	// 
-	constructor(width, height, content, bgColor) {
-		this.width = width;
-		this.height = height;
-		this.content = content;
-		this.bgColor = bgColor; 
+  //
+  constructor(width, height, content, bgColor) {
+    this.width = width;
+    this.height = height;
+    this.content = content;
+    this.bgColor = bgColor;
 
-		this.margin = 10;	// margin to be set around the artboard
-		this.objects = [];
+    this.margin = 10; // margin to be set around the artboard
+    this.objects = [];
+  }
 
+  addObjects(objs) {
+    objs.forEach((obj) => {
+      this.objects.push(obj);
+    });
+  }
 
-	}
-
-	addObjects(objs) {
-        objs.forEach((obj) => {
-            this.objects.push(obj);
-        });
+  calcOrientation(scrW, scrH) {
+    var m = this.margin * 2;
+    if (scrW / scrH > this.width / this.height) {
+      var orient = "v";
+      var artH = scrH - m;
+      var artW = this.width * (artH / this.height);
+      var baseW = (scrW - artW) / 2;
+      var baseH = this.margin;
+    } else if (scrW / scrH < this.width / this.height) {
+      var orient = "h";
+      var artW = scrW - m;
+      var artH = this.height * (artW / this.width);
+      var baseW = this.margin;
+      var baseH = (scrH - artH) / 2;
+    } else {
+      var orient = "sq";
+      if (scrW >= scrH) {
+        var artW = scrW - m;
+        var artH = this.height * (this.width / scrW);
+      } else {
+        var artH = scrH - m;
+        var artW = this.width * (this.height / scrH);
+      }
     }
 
-
-	calcOrientation(scrW, scrH) {
-		var m = this.margin * 2;
-		if ((scrW/scrH) > (this.width/this.height)) {
-			var orient = 'v';
-			var artH = scrH - m;
-			var artW = this.width * (artH/this.height);
-            var baseW = (scrW-artW)/2;
-            var baseH = this.margin;
-		} else if ((scrW/scrH) < (this.width/this.height)) {
-			var orient = 'h';
-			var artW = scrW - m;
-			var artH = this.height * (artW/this.width);
-            var baseW = this.margin;
-            var baseH = (scrH-artH)/2;
-		} else {
-			var orient = 'sq'
-			if (scrW >= scrH) {
-				var artW = scrW - m;
-				var artH = this.height * (this.width/scrW);
-			} else {
-				var artH = scrH - m;
-				var artW = this.width * (this.height/scrH);
-			}
-		}
-        
-        if (orient === 'v'){
-             var pixelRatio = artW/this.width;
-        } else {
-             var pixelRatio = artH/this.height;
-        }
-
-		return {width: artW, height: artH, orient: orient, baseCoord: {w: baseW, h: baseH}, pixelRatio: pixelRatio};
-	}
-
-	drawArtboard(context, artMeta) {
-        console.log("orientation: ", artMeta.orient);
-        
-		context.fillStyle = this.bgColor; 	
-		context.fillRect(artMeta.baseCoord.w, artMeta.baseCoord.h, artMeta.width, artMeta.height);
-	}
-
-    drawObjects(context, artMeta) {
-        console.log(artMeta.pixelRatio);
-
-        this.objects.forEach((obj) => {
-            obj.render(context, artMeta.pixelRatio, artMeta.baseCoord);
-        });
+    if (orient === "v") {
+      var pixelRatio = artW / this.width;
+    } else {
+      var pixelRatio = artH / this.height;
     }
 
-	draw(context) {
-        // reset canvas
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
+    return {
+      width: artW,
+      height: artH,
+      orient: orient,
+      baseCoord: { w: baseW, h: baseH },
+      pixelRatio: pixelRatio,
+    };
+  }
 
+  drawArtboard(context, artMeta) {
+    console.log("orientation: ", artMeta.orient);
 
-		var artMeta = this.calcOrientation(context.canvas.width, context.canvas.height);
+    context.fillStyle = this.bgColor;
+    context.fillRect(
+      artMeta.baseCoord.w,
+      artMeta.baseCoord.h,
+      artMeta.width,
+      artMeta.height
+    );
+  }
 
-		this.drawArtboard(context, artMeta);		
-        this.drawObjects(context, artMeta);
-	
-		
-	}
+  drawObjects(context, artMeta) {
+    console.log(artMeta.pixelRatio);
+
+    this.objects.forEach((obj) => {
+      obj.render(context, artMeta.pixelRatio, artMeta.baseCoord);
+    });
+  }
+
+  draw(context) {
+    // reset canvas
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    var artMeta = this.calcOrientation(
+      context.canvas.width,
+      context.canvas.height
+    );
+
+    this.drawArtboard(context, artMeta);
+    this.drawObjects(context, artMeta);
+  }
 }
-
 
 export default Artboard;
--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,26 +1,29 @@
 import React, { useRef, useEffect, useState } from "react";
-import Artboard from "./Artboard.js";
+import Artboard from "./Artboard";
+import Toolbox from "./Toolbox";
 import BaseObject from "./Objects/BaseObject";
 import "./Canvas.css";
-
-
+const colors = require("../colors.json");
 
 const Canvas = (props) => {
-	
-	let Doc = new Artboard(2100, 2970, [], '#dddddd');
-    
-  Doc.addObjects([new BaseObject(200, 200, 1200, 600, '#FF0000'), new BaseObject(0, 0, 100, 100, '#00DD00'), new BaseObject(1600, 2200, 400, 600, '#3333FF')]);
+  let Doc = new Artboard(2100, 2970, [], "#dddddd");
+  let Tools = new Toolbox();
 
+  Doc.addObjects([
+    new BaseObject(200, 200, 1200, 600, "#FF0000"),
+    new BaseObject(0, 0, 100, 100, "#00DD00"),
+    new BaseObject(1600, 2200, 400, 600, "#3333FF"),
+  ]);
 
   const [dimensions, setDimensions] = useState({
     height: window.innerHeight,
     width: window.innerWidth,
   });
 
-	//TODO: find a way to combine react & electron so that the electron.js win events etc. can be accessed in react	
-	//const [maximized, setMaximized] = useState({
-		//maximized: window.isMaximized()
-		//})
+  //TODO: find a way to combine react & electron so that the electron.js win events etc. can be accessed in react
+  //const [maximized, setMaximized] = useState({
+  //maximized: window.isMaximized()
+  //})
 
   const canvasRef = useRef(null);
 
@@ -30,30 +33,31 @@ const Canvas = (props) => {
     const context = canvas.getContext("2d");
 
     function handleResize() {
-			// TODO: limit the amount of resizes for performace purposes
+      // TODO: limit the amount of resizes for performace purposes
       setDimensions({
         height: window.innerHeight,
         width: window.innerWidth,
       });
-			//setMaximized({
-				//maximized: window.isMaximized()
-				//})
+      //setMaximized({
+      //maximized: window.isMaximized()
+      //})
       console.log("resize", dimensions.width, "x", dimensions.height);
 
-	//  const { width, height } = canvas.getBoundingClientRect();	
+      //  const { width, height } = canvas.getBoundingClientRect();
       canvas.width = dimensions.width;
       canvas.height = dimensions.height;
       Doc.draw(context);
+      Tools.render(context);
     }
     Doc.draw(context);
-
+    Tools.render(context);
     console.log("update");
 
     window.addEventListener("resize", handleResize);
-		//window.addEventListener("maximize", handleResize);
+    //window.addEventListener("maximize", handleResize);
     return (_) => {
       window.removeEventListener("resize", handleResize);
-			//window.addEventListener("maximize", handleResize);
+      //window.addEventListener("maximize", handleResize);
     };
     // clean up: remove listener to avoid memory leak by making sure there is always only one listener (every time the useEffect is called because of a resize event, a nev listener would be created)
 
@@ -61,7 +65,12 @@ const Canvas = (props) => {
     // second arguement([]): all items to be watched for changes, which result in recurring execution of the useEffect callback
   });
   return (
-    <canvas ref={canvasRef} width={dimensions.width} height={dimensions.height} id="theCanvas">
+    <canvas
+      ref={canvasRef}
+      width={dimensions.width}
+      height={dimensions.height}
+      id="theCanvas"
+    >
       {" "}
     </canvas>
   );
