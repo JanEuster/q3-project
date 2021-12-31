377eee662f2de5ccb4e7a6879543cb1d9917b076
JanEuster <jan.eusterschulte@rhgy.de>
2021-12-31 14:21:33 +0100

    remove debug pencil tool red dots

--- src/components/Canvas/Tools.js
+++ src/components/Canvas/Tools.js
@@ -119,7 +119,7 @@ class PencilTool {
     );
     if (e.type == "mousedown") {
       this.inUse = true;
-      Doc.addObject(new Circle(coords.x, coords.y, 8, "red", undefined, 0));
+      // Doc.addObject(new Circle(coords.x, coords.y, 8, "red", undefined, 0));
       this.currentPath = new Path(
         [[coords.x, coords.y]],
         ToolManager.strokeWidth,
@@ -132,7 +132,7 @@ class PencilTool {
 
     } else if (this.inUse && e.type == "mouseup") {
       this.currentPath.addPoints(coords.x, coords.y);
-      Doc.addObject(new Circle(coords.x, coords.y, 8, "red", undefined, 0));
+      // Doc.addObject(new Circle(coords.x, coords.y, 8, "red", undefined, 0));
 
       this.inUse = false;
       this.currentPath = NaN;
@@ -159,7 +159,7 @@ class Eraser extends PencilTool {
 
     if (e.type == "mousedown") {
       this.inUse = true;
-      Doc.addObject(new Circle(coords.x, coords.y, this.radius, "red", undefined, 0)); //colour --> getBackgroundColour Artboard Class
+      // Doc.addObject(new Circle(coords.x, coords.y, this.radius, "red", undefined, 0)); //colour --> getBackgroundColour Artboard Class
       this.currentPath = new Path(
         [[coords.x, coords.y]],
         ToolManager.strokeWidth,
@@ -170,7 +170,7 @@ class Eraser extends PencilTool {
       this.currentPath.addPoint(coords.x,coords.y);
     } else if(this.inUse && e.type == "mouseup") {
       this.currentPath.addPoints(coords.x,coords.y);
-      Doc.addObject(new Circle(coords.x, coords.y, this.radius, "red", undefined, 0));
+      // Doc.addObject(new Circle(coords.x, coords.y, this.radius, "red", undefined, 0));
 
       this.inUse = false;
       this.currentPath = NaN;
