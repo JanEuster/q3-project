83604534932db90efb7dc8f80508001c6c5b974b
JanEuster <jan.eusterschulte@rhgy.de>
2022-01-03 23:52:41 +0100

    add boundbox outline for debug

--- src/components/Canvas/Objects/BoundingBox.js
+++ src/components/Canvas/Objects/BoundingBox.js
@@ -54,6 +54,19 @@ class BoundingBox {
       return false;
     }
   }
+
+  render(context, offset={x:0, y:0}) {
+    // render bounding box for testing
+    context.strokeStyle = "#FF0000"
+    context.lineWidth = 3
+
+    context.strokeRect(
+      offset.x + this.coords[0],
+      offset.y + this.coords[1],
+      this.wh[0],
+      this.wh[1]
+    )
+  }
 }
 
 export default BoundingBox;
