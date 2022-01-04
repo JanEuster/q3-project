3d24f676f23c7475016f1e5eb40a211b67486ef4
JanEuster <jan.eusterschulte@rhgy.de>
2022-01-04 18:12:09 +0100

    Updates

---
+++ precache-manifest.f901450405a2ef6259a05541bebae907.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac953c408931bc13408eda737fdc4bb7",
+    "url": "/expanded-board/index.html"
+  },
+  {
+    "revision": "9416a93b96cbb675ac37",
+    "url": "/expanded-board/static/css/main.a7be353e.chunk.css"
+  },
+  {
+    "revision": "fd5d2abfe6a42c5a3c6e",
+    "url": "/expanded-board/static/js/2.a82b2819.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/expanded-board/static/js/2.a82b2819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9416a93b96cbb675ac37",
+    "url": "/expanded-board/static/js/main.f5ac4521.chunk.js"
+  },
+  {
+    "revision": "a7316e1cc14c99be6c01",
+    "url": "/expanded-board/static/js/runtime-main.36be2017.js"
+  },
+  {
+    "revision": "cb7bfe988b1e0ca8268226e20a80a491",
+    "url": "/expanded-board/static/media/iosevka-regular.cb7bfe98.ttc"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/expanded-board/static/media/logo.06e73328.svg"
+  }
+]);
