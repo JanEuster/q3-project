0dc69bd349dbabcfec58f2376b4cb0aaf1472828
JanEuster <jan.eusterschulte@rhgy.de>
2022-01-04 14:09:58 +0100

    add panel sticky to right/bottom side (via negative panel coords)

--- src/components/Canvas/Panels/BasePanel.js
+++ src/components/Canvas/Panels/BasePanel.js
@@ -16,24 +16,37 @@ class Panel {
         this.border = border
         this.bhalf = this.border/2
 
-        this.rightSided = rightSided // determines whether panel sticks to top left or bottom right side of the window
-
         this.components = [ new PanelButton( 20, 100, 20, 20 ), new PanelTitle( 20, 30, "Settings" ), new PanelText( 20, 50, "Settings" ), new PanelTextSwitch( 20, 80, "Settings" )]
 
         this.boundingBox = new BoundingBox(this.x - this.bhalf, this.y - this.bhalf, this.width + this.border, this.height + this.border)
     }
     
     checkBoundsCollision(x, y) { 
-        this.boundingBox = new BoundingBox(this.x - this.bhalf, this.y - this.bhalf, this.width + this.border, this.height + this.border)
+        let panelXY = this.getXY()
+
+        this.boundingBox = new BoundingBox(panelXY.x - this.bhalf, panelXY.y - this.bhalf, this.width + this.border, this.height + this.border)
         if (this.boundingBox.checkCollision(x, y)) {
             this.components.map(comp => {
-                if (comp.boundingBox.checkCollision(x - this.x, y - this.y)) comp.handleColission()
+                if (comp.boundingBox.checkCollision(x - panelXY.x, y - panelXY.y)) comp.handleColission()
             })
             return true
         }
         return false
     }
     
+    getXY() {
+        // if panel x or y coords are negative, the panel is sticky towards that bottom/ right side
+        let x, y;
+
+        if (this.x < 0) {
+            x = window.innerWidth + this.x
+        } else x = this.x 
+        if (this.y < 0) {
+            y = window.innerHeight + this.y
+        } else y = this.y
+        return {x: x, y: y}
+    }
+
     setXY() {
         // set x and y base values on resize
     }
@@ -42,9 +55,9 @@ class Panel {
         this.setXY()
     }
 
-    renderComponents(context) {
+    renderComponents(context, panelXY) {
         this.components.map(comp => {
-            let panelOffset = {x: this.x, y: this.y} // panel offset to 
+            let panelOffset = panelXY // {x: x, y: y} panel offset to 
             comp.render(context, panelOffset)
         })
     }
@@ -53,10 +66,12 @@ class Panel {
         context.strokeStyle = colors.darkgrey
         context.lineWidth = this.border
 
-        context.fillRect(this.x, this.y, this.width, this.height)
-        context.strokeRect(this.x, this.y, this.width, this.height)
+        let panelXY = this.getXY()
 
-        this.renderComponents(context)
+        context.fillRect(panelXY.x, panelXY.y, this.width, this.height)
+        context.strokeRect(panelXY.x, panelXY.y, this.width, this.height)
+
+        this.renderComponents(context, panelXY)
     }
 }
 
