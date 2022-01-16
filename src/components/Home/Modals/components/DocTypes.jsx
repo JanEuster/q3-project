d939c54ad3aef2e888a1567e4daf4bc2fc18d1c9
JanEuster <jan.eusterschulte@rhgy.de>
2022-01-16 19:26:36 +0100

    fix gh pages svg public assets path

--- src/components/Home/Modals/components/DocTypes.jsx
+++ src/components/Home/Modals/components/DocTypes.jsx
@@ -35,8 +35,8 @@ class DocTypeSelector extends Component {
           src={
             this.state.docType === this.props.docTypes[0]
               ? process.env.PUBLIC_URL +
-                "assets/icons/ui/regular_doc-selected.svg"
-              : process.env.PUBLIC_URL + "assets/icons/ui/regular_doc.svg"
+                "/assets/icons/ui/regular_doc-selected.svg"
+              : process.env.PUBLIC_URL + "/assets/icons/ui/regular_doc.svg"
           }
           onClick={() => this.setState({ docType: this.props.docTypes[0] })}
           style={{ width: this.props.width, gridColumn: 1 }}
@@ -45,9 +45,9 @@ class DocTypeSelector extends Component {
           src={
             this.state.docType === this.props.docTypes[1]
               ? process.env.PUBLIC_URL +
-                "assets/icons/ui/infinite_scroll_doc-selected.svg"
+                "/assets/icons/ui/infinite_scroll_doc-selected.svg"
               : process.env.PUBLIC_URL +
-                "assets/icons/ui/infinite_scroll_doc.svg"
+                "/assets/icons/ui/infinite_scroll_doc.svg"
           }
           onClick={() => this.setState({ docType: this.props.docTypes[1] })}
           style={{ width: this.props.width, gridColumn: 2 }}
@@ -56,8 +56,8 @@ class DocTypeSelector extends Component {
           src={
             this.state.docType === this.props.docTypes[2]
               ? process.env.PUBLIC_URL +
-                "assets/icons/ui/infinite_doc-selected.svg"
-              : process.env.PUBLIC_URL + "assets/icons/ui/infinite_doc.svg"
+                "/assets/icons/ui/infinite_doc-selected.svg"
+              : process.env.PUBLIC_URL + "/assets/icons/ui/infinite_doc.svg"
           }
           onClick={() => this.setState({ docType: this.props.docTypes[2] })}
           style={{ width: this.props.width, gridColumn: 3 }}
